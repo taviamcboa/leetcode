9d37eab6a1fb23775d9a385fdcd5beb9b3e1e03a
unknown <chengxiao1985@gmail.com>
2017-09-24 16:35:09 -0500

    1d dp

---
+++ 674/main.cpp
@@ -0,0 +1,18 @@
+
+/* 
+Input: [1,3,5,4,7]
+Output: 3
+Explanation: The longest continuous increasing subsequence is [1,3,5], its length is 3. 
+Even though [1,3,5,7] is also an increasing subsequence, it's not a continuous one where 5 and 7 are separated by 4. 
+
+ */
+class Solution {
+public:
+    int findLengthOfLCIS(vector<int>& nums) {
+        
+
+
+
+
+    }
+};
