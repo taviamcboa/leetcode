ed0c081a2e19edc9b33990417fe8a04a06b369a8
unknown <chengxiao1985@gmail.com>
2017-09-24 14:53:01 -0500

    update code

--- 682/main.cpp
+++ 682/main.cpp
@@ -1,28 +1,49 @@
 class Solution {
 public:
     int calPoints(vector<string>& ops) {
+        int result = 0; 
         
-
-
-
-
-
-
-
-
-
-
-
+        //double linked list
+        list<int> validInput; 
+        for(int i = 0; i < ops.size(); i++){
+            int currentRoundPts; 
+            const string input = ops[i];
+            if(isNum(input)){
+                currentRoundPts = std::stoi(input);
+                validInput.push_front(currentRoundPts); 
+            }else{
+                if(input == "+"){
+                        if(validInput.size() < 2){
+                            throw "invalid input";
+                        }
+                        currentRoundPts = validInput.front() + *(++validInput.begin());
+                        validInput.push_front(currentRoundPts);
+                }else if(input == "D"){         
+                        currentRoundPts = validInput.front() * 2;
+                        validInput.push_front(currentRoundPts);
+                }else if(input == "C"){
+                        currentRoundPts = -validInput.front();
+                        validInput.pop_front();
+                }             
+            }
+            result += currentRoundPts; 
+        }
+        return result;
+    }
+    
+    
+private: 
+    bool isNum(const string& word){
+        bool isNumber = true;
+        if(word[0] == '-'){
+            for(string::const_iterator k = ++word.begin(); k != word.end(); ++k){
+                isNumber = isNumber && isdigit(*k);
+            }
+        }else{
+            for(string::const_iterator k = word.begin(); k != word.end(); ++k){
+                isNumber = isNumber && isdigit(*k);
+            }
+        }
+        return isNumber;
     }
 };
-
-
-
-void main(){
-    Solution sol; 
-
-    
-
-    sol.calPoints()
-}
-
